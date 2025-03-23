112195c4d45bf262ca3caef46b76175828020205
RobinJuul <robin.juul@ut.ee>
2025-03-23 13:09:08 +0200

    .xml update

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -4,13 +4,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 
 public class ClientHandler implements Runnable {
     private static final int TYPE_ECHO = 1;
-    private static final int TYPE_FILE = 2;
     private static final int OK = 200;
     private static final int ERROR = 400;
     private final Socket socket;
@@ -21,39 +18,31 @@ public class ClientHandler implements Runnable {
 
     @Override
     public void run() {
-        long threadId = Thread.currentThread().threadId();
-        System.out.println("Thread " + threadId + " started");
+        System.out.println("ClientHandler started");
 
         try (Socket s = socket;
              DataInputStream din = new DataInputStream(s.getInputStream());
              DataOutputStream dout = new DataOutputStream(s.getOutputStream())) {
 
-            int messageCount = din.readInt();
-            for (int i = 0; i < messageCount; i++) {
+            while (true) {
+                try{
                 int request = din.readInt();
+                System.out.println("Received request type: " + request);
                 if (request == TYPE_ECHO) {
                     String message = din.readUTF();
                     dout.writeInt(OK);
                     dout.writeUTF(message);
                     System.out.println("ECHO: " + message);
 
-                } else if (request == TYPE_FILE) {
-                    String filename = din.readUTF();
-                    Path path = Paths.get(filename);
-                    if (!path.isAbsolute() && Files.isRegularFile(path)) {
-                        dout.writeInt(OK);
-                        byte[] fileContent = Files.readAllBytes(path);
-                        dout.writeInt(fileContent.length);
-                        dout.write(fileContent);
-                        System.out.println("FILE: " + filename);
-                    } else {
-                        dout.writeInt(ERROR);
-                    }
                 } else {
                     dout.writeInt(ERROR);
                 }
+                } catch (IOException e) {
+                    System.out.println("Connection with client lost.");
+                    break;
+                }
             }
-            System.out.println("Thread " + threadId + " ended");
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
