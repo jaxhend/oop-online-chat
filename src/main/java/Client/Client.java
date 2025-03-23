1ec86474c1988ded19a6b2232dd098532f7b50cd
Hendrik Jaks <hendrikjaks@gmail.com>
2025-03-23 14:02:24 +0200

    added deploy.yml

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -14,8 +14,8 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Connecting to server");
-        // server: 35.212.230.59
-        try (Socket socket = new Socket("35.212.230.59", 45367);
+        // server: 35.212.230.59 ehk oop.atlante.ee
+        try (Socket socket = new Socket("oop.atlante.ee", 45367);
             DataInputStream din = new DataInputStream(socket.getInputStream());
             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
             Scanner scanner = new Scanner(System.in)) {
