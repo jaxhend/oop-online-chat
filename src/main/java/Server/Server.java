5550dc473f29ca0f724bd3ef43ee268df0cb51c0
RobinJuul <robin.juul@ut.ee>
2025-03-23 10:59:48 +0200

    updated port to 45367

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -7,8 +7,8 @@ import java.net.Socket;
 public class Server {
 
     public static void main(String[] args) throws IOException {
-        try (ServerSocket ss = new ServerSocket(1337)) {
-            System.out.println("Now listening on :1337");
+        try (ServerSocket ss = new ServerSocket(45367)) {
+            System.out.println("Now listening on :45367");
             System.out.println("Current working directory: " + System.getProperty("user.dir"));
 
             while (true) {
