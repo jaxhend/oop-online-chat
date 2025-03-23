0a4fb29d205f1d3c9adde596e4c6fa2d67d52352
RobinJuul <robin.juul@ut.ee>
2025-03-23 10:20:18 +0200

    added Server template

---
+++ src/main/java/Server/Server.java
@@ -0,0 +1,22 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket ss = new ServerSocket(1337)) {
+            System.out.println("Now listening on :1337");
+            System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+            while (true) {
+                Socket s = ss.accept();
+                System.out.println("Connected: " + s.getInetAddress());
+                Thread client = new Thread(new ClientHandler(s));
+                client.start();
+            }
+        }
+    }
+}
