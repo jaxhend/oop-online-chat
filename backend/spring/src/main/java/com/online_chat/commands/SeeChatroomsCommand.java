c0804d85e8e550fa9eac311409ef5ca6c6704f39
RobinJuul <robin.juul@ut.ee>
2025-05-01 18:39:38 +0300

    Fix to make sure default rooms are always displayed first

--- backend/spring/src/main/java/com/online_chat/commands/SeeChatroomsCommand.java
+++ backend/spring/src/main/java/com/online_chat/commands/SeeChatroomsCommand.java
@@ -6,6 +6,9 @@ import com.online_chat.model.ClientSession;
 
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.online_chat.model.ChatRoomManager.defaultRooms;
 
 public class SeeChatroomsCommand implements Command {
 
@@ -19,11 +22,15 @@ public class SeeChatroomsCommand implements Command {
     public String execute(ClientSession session, String[] args) {
         Set<String> roomNames = chatRoomManager.getRoomNames();
 
-        String result = roomNames.stream()
-                .filter(name -> !name.toLowerCase().contains(":"))
-                .map(name -> name + " (" + chatRoomManager.getRoom(name).getClientsCount() + ")")
-                .collect(Collectors.joining(", "));
+        String result = Stream.concat(
+                defaultRooms.stream()
+                        .filter(roomNames::contains)
+                        .map(name -> name + " (" + chatRoomManager.getRoom(name).getClientsCount() + ")"),
 
+                roomNames.stream()
+                    .filter(name -> !defaultRooms.contains(name) && !name.toLowerCase().contains(":"))
+                    .map(name -> name + " (" + chatRoomManager.getRoom(name).getClientsCount() + ")")
+        ).collect(Collectors.joining(", "));
         return "Saadaval vestlusruumid: " + result;
     }
 
