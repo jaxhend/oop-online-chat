7f92e17782a4e5193fa588c4f9acb79a4657ccc4
KarlKiudma <kiudmakarlmarkus@gmail.com>
2025-05-01 18:53:58 +0300

    React changes

--- backend/spring/src/main/java/com/online_chat/commands/HelpCommand.java
+++ backend/spring/src/main/java/com/online_chat/commands/HelpCommand.java
@@ -8,7 +8,6 @@ public class HelpCommand implements Command {
     @Override
     public String execute(ClientSession session, String[] args) {
         return "Saadaolevad käsud:\n\n"
-                + "/username <nimi> – muudab sinu kasutajanime\n\n"
                 + "/join <ruum> – liitub või loob uue vestlusruumi\n\n"
                 + "/leave – lahkub aktiivsest vestlusruumist\n\n"
                 + "/chatrooms – kuvab avalikud ruumid ja aktiivsete liikmete arvu\n\n"
