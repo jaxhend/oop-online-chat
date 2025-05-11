07f9727ef89641299c4833d9d071b74af15a0015
RobinJuul <robin.juul@ut.ee>
2025-05-11 13:23:10 +0300

    improved visual structure for username dialog

--- frontend/src/components/UsernameDialog/UsernameDialog.jsx
+++ frontend/src/components/UsernameDialog/UsernameDialog.jsx
@@ -6,21 +6,24 @@ import utLogo from '../../assets/ut-logo.png';
 export default function UsernameDialog({ onSubmit, error }) {
     return (
         <div className={styles.overlay}>
-            <div className="bg-white text-black dark:bg-zinc-900 dark:text-white p-6 rounded-2xl shadow-xl w-full max-w-md">
-                <h2 className={styles.utname}>UTCHAT</h2>
-                <img
-                    className={styles.utlogo}
-                    src={utLogo}
-                    alt="UT logo"
-                />
-                <h2 className="text-xl font-semibold mb-4 text-center">Sisesta kasutajanimi</h2>
-
-                <TerminalInput
-                    isActive={true}
-                    onSubmit={onSubmit}
-                    showEmojiButton={false}
-                    placeholder={"Sisesta kasutajanimi..."}
-                />
+            <div className={styles["center-wrapper"]}>
+                <div className={styles["branding"]}>
+                    <h1 className={styles["branding-title"]}>UTCHAT</h1>
+                    <img
+                        className={styles.utlogo}
+                        src={utLogo}
+                        alt="UT logo"
+                    />
+                </div>
+                <div className={styles["username-entry"]}>
+                    <h2 className={styles["username-title"]}>Sisesta kasutajanimi</h2>
+                    <TerminalInput
+                        isActive={true}
+                        onSubmit={onSubmit}
+                        showEmojiButton={false}
+                        placeholder={"Sisesta kasutajanimi..."}
+                    />
+                </div>
 
                 {error && <p className={styles["error-text"]}>{error}</p>}
             </div>
