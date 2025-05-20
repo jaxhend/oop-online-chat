1fc35dab93a72080fb1e4e7a9d0ded947a329e22
RobinJuul <robin.juul@ut.ee>
2025-05-20 12:17:41 +0300

    dailydeals improvement

--- frontend/src/components/DailyDeals/DailyDeals.jsx
+++ frontend/src/components/DailyDeals/DailyDeals.jsx
@@ -16,6 +16,7 @@ export default function DailyDeals({deals}) {
 
     return (
         <motion.div
+            className={styles[""]}
             initial={{opacity: 0, y: 20}}
             animate={{opacity: 1, y: 0}}
             transition={{duration: 0.6}}
